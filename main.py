d4915e619a4818c5c72997f8a1c0cfb393165276
wlinds <w.lindstedt@gmail.com>
2024-11-10 22:48:37 +0100

    .

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from data_cleaner import *
 from viz import *
-
+from maps import maps
 from utils import search_df
 
 def main():
 
     configs = [
         ColumnConfig('example_column_age', 'ålder_clean', '', ColumnType.NUMBER),
-        ColumnConfig('example_column_work_title', 'yrkesroll_cat', 'yrkesroller', ColumnType.SINGLE),
+        ColumnConfig('example_column_work_title', 'yrkesroll_cat', 'yrkesroll', ColumnType.SINGLE),
         ColumnConfig('example_column_yes_no_but_its_not_a_choice_but_free_text', 'yes_no_clean', 'ja_nej_svar', ColumnType.YES_NO),
         ColumnConfig('example_column_time', 'tillräcklig_tid_cat', 'ja_nej_svar', ColumnType.YES_NO),
         ColumnConfig('example_column_places', 'platser_cat', 'när_var', ColumnType.MULTIPLE),
@@ -16,23 +16,18 @@ def main():
         ColumnConfig('example_column_values', 'värde_cat', 'värde', ColumnType.MULTIPLE),
     ]
 
-    df = pd.read_excel("data/svar.xlsx")
     df = pd.read_excel("example_data/example_sv.xlsx")
 
-    cleaner = DataCleaner(configs)
-    df_processed = cleaner.process_df(df)
-
-    analyzer = DataAnalyzer()
-    results = analyzer.analyze_categories(df_processed, configs)
+    cleaner = DataCleaner(configs, maps)
+    df_processed = cleaner.process_dataframe(df)
+    results = DataCleaner.analyze_categories(df_processed, configs)
+    DataCleaner.save_results(df_processed)
     
     for col_name, distribution in results.items():
         print(f"\n{col_name}:")
         print(distribution)
-    
-    analyzer.save_results(df_processed)
 
     # Visualizations
-
     create_visualizations(df_processed)
     create_topic_value_graphs(df_processed)
 
